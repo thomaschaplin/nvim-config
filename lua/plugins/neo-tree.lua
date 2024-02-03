fa5df2cba8642d3f3c6b93b11a1ede4a7350d749
Thomas Chaplin <thomaschaplin@outlook.com>
2024-02-03 23:51:50 +0000

    add opts to neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,8 +6,32 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          '.git',
+          'node_modules'
+        },
+        hide_by_pattern = {
+          "**/node_modules/*",
+        },
+        never_show = {
+          ".DS_Store",
+          "thumbs.db"
+        },
+        always_show = {
+          ".gitignored",
+        }
+      },
+    }
+  },
   config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+  end,
 }
