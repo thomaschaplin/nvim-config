420257027a13861b1e097a63fe93736a19a04600
Thomas Chaplin <thomaschaplin@outlook.com>
2024-02-03 13:04:54 +0000

    formatting

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,7 +14,7 @@ vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
-vim.keymap.set("n", "<C-z>", ":undo <CR>")
+vim.keymap.set("n", "<C-z>", ":undo<CR>")
 
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
