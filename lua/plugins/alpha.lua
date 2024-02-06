84ee96e137823a1e62e2d4d1d89e5757504c2743
Thomas Chaplin <thomaschaplin@outlook.com>
2024-02-06 10:17:42 +0000

    update dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,42 +1,43 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
 
-		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
 
-		dashboard.section.buttons.val = {
-			dashboard.button("n", "🆕  > New file", ":ene<CR>"),
-			dashboard.button("f", "🔍  > Find file", ":cd $HOME/GIT | Telescope find_files<CR>"),
-			dashboard.button("r", "📌  > Recent", ":Telescope oldfiles <CR>"),
-			dashboard.button("p", "🗂️  > Projects", ":cd $HOME/git | Neotree filesystem toggle float<CR>"),
-			dashboard.button("s", "⚙️   > Settings", ":cd ~/.config/nvim | Neotree filesystem toggle float<CR>"),
-			dashboard.button("l", "💤  > Lazy", ":Lazy<CR>"),
-			dashboard.button("q", "⛔️  > Quit NVIM", ":qa<CR>"),
-		}
+    dashboard.section.buttons.val = {
+      dashboard.button("n", "🆕  > New file", ":ene<CR>"),
+      dashboard.button("g", "🔍  > Find file (git)", ":cd $HOME/GIT | Telescope find_files<CR>"),
+      dashboard.button("f", "🔍  > Find file (cwd)", ":Telescope find_files<CR>"),
+      dashboard.button("r", "📌  > Recent", ":Telescope oldfiles <CR>"),
+      dashboard.button("p", "🗂️  > Projects", ":cd $HOME/git | Neotree filesystem toggle float<CR>"),
+      dashboard.button("s", "⚙️   > Settings", ":cd ~/.config/nvim | Neotree filesystem toggle float<CR>"),
+      dashboard.button("l", "💤  > Lazy", ":Lazy<CR>"),
+      dashboard.button("q", "⛔️  > Quit NVIM", ":qa<CR>"),
+    }
 
-		dashboard.section.footer.val = 'Success - "We cannot direct the wind but we can adjust the sails"'
+    dashboard.section.footer.val = 'Success - "We cannot direct the wind but we can adjust the sails"'
 
-		alpha.setup(dashboard.opts)
-	end,
+    alpha.setup(dashboard.opts)
+  end,
 }
