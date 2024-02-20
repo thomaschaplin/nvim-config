eecbbf62d77b4b5ab5f22d68e5cc5de6ddb2fd00
Thomas Chaplin <thomaschaplin@outlook.com>
2024-02-20 17:06:12 +0000

    configure harpoon to use telescope

--- lua/thomaschaplin/options.lua
+++ lua/thomaschaplin/options.lua
@@ -89,13 +89,13 @@ vim.opt.ruler = true
 vim.keymap.set("n", "<C-z>", ":undo<CR>")
 
 -- Remove highlight
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.keymap.set("n", "<leader>hi", ":nohlsearch<CR>")
 
 -- END
 
 -- Navigate vim panes better
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+-- vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+-- vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+-- vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+-- vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
