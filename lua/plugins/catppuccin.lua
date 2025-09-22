e0db848a8ac9af33f5ae63c70b65c43a39721589
Thomas Chaplin <thomaschaplin@outlook.com>
2025-09-22 22:48:43 +0100

    updates

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,39 +5,12 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
+      auto_integrations = true,
       flavour = "mocha",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = false,
-      dim_inactive = {
-        enabled = true,
-        shade = "dark",
-        percentage = 0.15,
-      },
       styles = {
         comments = { "italic" },
         conditionals = { "italic" },
-      },
-      custom_highlights = function(colors)
-        return {
-          -- Snacks picker
-          SnacksPickerNormal = { bg = colors.mantle },
-          SnacksPickerBorder = { bg = colors.mantle, fg = colors.blue },
-          SnacksPickerSelection = { bg = colors.surface0, fg = colors.text },
-        }
-      end,
-      integrations = {
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            warnings = { "italic" },
-          },
-        },
-        snacks = true,
-      },
+      }
     })
 
     vim.cmd.colorscheme("catppuccin")
